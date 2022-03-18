8647b541a54f762e92ea51f28a6e6aea30741f6a
alievhalid <s7600180@gmail.com>
2022-03-18 19:24:53 +0300

    header done

--- src/common/App.js
+++ src/common/App.js
@@ -1,8 +1,9 @@
+import Header from "../components/header/Header";
 
 function App() {
   return (
     <div className="App">
-     appdsada
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./header.module.scss";
+import PersonAddAltRoundedIcon from "@mui/icons-material/PersonAddAltRounded";
+import AccessTimeFilledTwoToneIcon from "@mui/icons-material/AccessTimeFilledTwoTone";
+import TaskRoundedIcon from "@mui/icons-material/TaskRounded";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.inputs}>
+        <input type="text" placeholder="Поиск пациента" />
+      </div>
+      <nav className={styles.nav}>
+        <div className={styles.icons}>
+          <li>
+            <PersonAddAltRoundedIcon />
+          </li>
+          <li>Добавить пациента</li>
+        </div>
+        <div className={styles.icons}>
+          <li>
+            <AccessTimeFilledTwoToneIcon />
+          </li>
+          <li>Лист ожидания</li>
+        </div>
+        <div className={styles.icons}>
+          <li>
+            <TaskRoundedIcon />
+          </li>
+          <li>Задачи</li>
+        </div>
+        <div className={styles.icons}>
+          <li>
+            <AccountCircleOutlinedIcon />
+          </li>
+          <li>Халид</li>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
