cda96d319de6aa7e7c48c9f9cd6546e6b1f5ebb2
alievhalid <s7600180@gmail.com>
2022-03-21 11:32:45 +0300

    fixed content

--- src/common/App.js
+++ src/common/App.js
@@ -1,11 +1,12 @@
 import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
+import Content from "./Content";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Header />
+      <Content />
     </div>
   );
 }
