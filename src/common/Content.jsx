1cadf7fd45e480aaeec8236a5954d8718c05427a
alievhalid <s7600180@gmail.com>
2022-03-22 16:32:08 +0300

    calendar page done

--- src/common/Content.jsx
+++ src/common/Content.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Calendar from "../components/calendar/Calendar";
 import Header from "../components/header/Header";
-// import Patients from "../components/patients/Patients";
+import Patients from "../components/patients/Patients";
 import Staff from "../components/staff/Staff";
 import styles from "./content.module.scss";
 function Content() {
   return (
     <div className={styles.content}>
       <Header />
-      {/* <Patients /> */}
-      <Staff />
+      <Routes>
+        <Route path="staff" element={<Staff />} />
+        <Route path="patients" element={<Patients />} />
+        <Route path="calendar" element={<Calendar />} />
+      </Routes>
     </div>
   );
 }
