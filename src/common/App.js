d81110dc331a1309ec322ab4c3bae4c6f1ce1232
Mahammad <idmag95@gmail.com>
2022-03-23 14:07:34 +0300

    Создал компонент авторизации

--- src/common/App.js
+++ src/common/App.js
@@ -1,12 +1,14 @@
 import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
 import Content from "./Content";
+import Auth from "../components/auth/Auth";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <Content />
+        <Auth/>
+      {/*<Sidebar />*/}
+      {/*<Content />*/}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './common/App';
+import {Provider} from "react-redux";
+import store from './redux/index'
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
