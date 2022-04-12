78c9d581562364b3527a4e29cac4b3b596aa5ee0
Mahammad <idmag95@gmail.com>
2022-04-12 19:57:55 +0300

    changed login form

--- src/common/App.js
+++ src/common/App.js
@@ -1,15 +1,16 @@
 import Sidebar from "../components/sidebar/Sidebar";
 import Content from "./Content";
+import Auth from "../components/auth/Auth";
 // import Auth from "../components/auth/Auth";
 
 function App() {
   return (
     <div className="App">
-      {/* <Auth /> */}
-      <div className="content">
-        <Sidebar />
-        <Content />
-      </div>
+       <Auth />
+      {/*<div className="content">*/}
+      {/*  <Sidebar />*/}
+      {/*  <Content />*/}
+      {/*</div>*/}
     </div>
   );
 }
